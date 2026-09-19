abea4657b2faae09c080ed9662faddfdecec172b
Kartik Jain <101446500+kartikjainmrt@users.noreply.github.com>
2026-09-20 00:53:02 +0530

    Fix GitHub Pages CSS and asset paths

--- solesetu-frontend/next.config.ts
+++ solesetu-frontend/next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   reactCompiler: true,
+
+  basePath: "/solesetu",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
